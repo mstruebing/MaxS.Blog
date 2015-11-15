7d801a8ed55f6007a4007e7a1a48ae010dd564e5
Max Strübing <max_struebing@gmx.de>
2015-11-15 14:02:31 +0100

    [TASK] Add basic Post and Blog Models/Repositories

--- Classes/MaxS/Blog/Domain/Model/Post.php
+++ Classes/MaxS/Blog/Domain/Model/Post.php
@@ -6,6 +6,7 @@ namespace MaxS\Blog\Domain\Model;
  *                                                                        *
  *                                                                        */
 
+use \MaxS\Blog\Domain\Model\Blog;
 use TYPO3\Flow\Annotations as Flow;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -15,24 +16,118 @@ use Doctrine\ORM\Mapping as ORM;
 class Post {
 
 	/**
+	 * @Flow\Validate(type="NotEmpty")
+	 * @ORM\ManyToOne(inversedBy="posts")
+	 * @var Blog
+	 */
+	protected $blog;
+
+	/**
+   * @Flow\Validate(type="NotEmpty")
+   * @var string
+   */
+	protected $title;
+
+	/**
+	 * @var \DateTime
+	 */
+	protected $date;
+
+	/**
+	 * @Flow\Validate(type="NotEmpty")
 	 * @var string
 	 */
-	protected $name;
+	protected $author;
 
+	/**
+	 * @Flow\Validate(type="NotEmpty")
+	 * @ORM\Column(type="text")
+	 * @var string
+	 */
+	protected $content;
+
+	/**
+	 * Constructs this post
+	 */
+	public function __construct() {
+		$this->date = new \DateTime();
+	}
+
+
+	/**
+	 * @return Blog
+	 */
+	public function getBlog() {
+		return $this->blog;
+	}
+
+	/**
+	 * @param Blog $blog
+	 * @return void
+	 */
+	public function setBlog($blog) {
+		$this->blog = $blog;
+		$this->blog->addPost($his);
+	}
 
 	/**
 	 * @return string
 	 */
-	public function getName() {
-		return $this->name;
+	public function getTitle() {
+		return $this->title;
 	}
 
 	/**
-	 * @param string $name
+	 * @param string $title
 	 * @return void
 	 */
-	public function setName($name) {
-		$this->name = $name;
+	public function setTitle($title) {
+		$this->title = $title;
+	}
+
+	/**
+	 * @return \DateTime
+	 */
+	public function getDate() {
+		return $this->date;
+	}
+
+	/**
+	 * @param \DateTime $date
+	 * @return void
+	 */
+	public function setDate($date) {
+		$this->date = $date;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getAuthor() {
+		return $this->author;
+	}
+
+	/**
+	 * @param string $author
+	 * @return void
+	 */
+	public function setAuthor($author) {
+		$this->author = $author;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getContent() {
+		return $this->content;
+	}
+
+	/**
+	 * @param string $content
+	 * @return void
+	 */
+	public function setContent($content) {
+		$this->content = $content;
 	}
 
 }
